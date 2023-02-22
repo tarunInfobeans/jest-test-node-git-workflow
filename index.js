43551db3d93cd26ff635455b921212558400f29d
tarunInfobeans <tarun.rathore@infobeans.com>
2023-02-22 13:28:05 +0530

    again checking the test & linting5

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('HEY THERE xyz');
+    res.send('HEY THERE');
 })
 
 app.get('/first-route', (req, res) => {
